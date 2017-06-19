481b308437b3cd4dadd396f5fe6980d9ca70db4c
Kevin Mokoli <kevinmokoli@Kevin-Mokolis-MacBook-Pro.local>
2017-06-18 23:56:34 -0500

    update

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name,devoured) VALUES ("The Big Juicy",false)
+INSERT INTO burgers (burger_name,devoured) VALUES ("BBQ",false);
+INSERT INTO burgers (burger_name,devoured) VALUES ("Chickenator",false);
